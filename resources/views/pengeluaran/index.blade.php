e1a081d7a87065574c662f6fe79d757593112410
andrian saputra <Andrian.Saputra@hashmicro.com.sg>
2024-12-22 18:53:15 +0700

    Fix text

--- resources/views/pengeluaran/index.blade.php
+++ resources/views/pengeluaran/index.blade.php
@@ -193,7 +193,7 @@
                                 <svg class="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                                 </svg>
-                                <h3 class="mb-5 text-lg font-normal text-gray-500">Hapus Data Pemasukan?</h3>
+                                <h3 class="mb-5 text-lg font-normal text-gray-500">Hapus Data Pengeluaran?</h3>
                                 <form action="{{ route('pengeluaran.destroy', $data->id) }}" method="post">
                                     @csrf
                                     @method('delete')
